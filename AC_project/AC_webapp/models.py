946821945c153d884e96ab0d0ca551aa42f7d4b7
Justin Cooper <46832618+Cooperj7@users.noreply.github.com>
2020-06-24 01:33:06 -0400

    Add files via upload

--- AC_project/AC_webapp/models.py
+++ AC_project/AC_webapp/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+class Weather(models.Model):
+    date_time = models.CharField(max_length=40)
+    temperature = models.FloatField(default=0)
+    min_temperature = models.FloatField(default=0)
+    max_temperature = models.FloatField(default=0)
+    humidity = models.IntegerField(default=0)
+    weather_label = models.CharField(max_length=50)
+    weather_description = models.CharField(max_length=200)
+    cloudiness_level = models.IntegerField(default=0)
+    wind_speed = models.FloatField(default=0)
+
+class SensorData(models.Model):
+    water_pH = models.FloatField(default=0)
+    tds = models.IntegerField(default=0)
+    water_temp = models.FloatField(default=0)
+    air_temp = models.FloatField(default=0)
+    humidity = models.FloatField(default=0)
+
+    
+    
+    
+
+    
+    
--- AC_project/AC_webapp/views.py
+++ AC_project/AC_webapp/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def Homepage(request):
+
+    context = {
+        }
+
+    """ 'water_pH'=water_pH,
+        'tds'=tds,
+        'water_temp'=water_temp,
+        'air_temp'=air_temp,
+        'humidity'=humidity,
+        'date_time'=date_time,
+        'temperature'=temperature,
+        'min_temperature'=min_temperature,
+        'max_temperature'=max_temperature,
+        'humidity'=humidity,
+        'weather_label'=weather_label,
+        'weather_description'=weather description,
+        'cloudiness_level'=cloudiness level,
+        'wind_speed'=wind speed,
+        """
+
+    return render(request, 'Homepage.htm', context=context)
