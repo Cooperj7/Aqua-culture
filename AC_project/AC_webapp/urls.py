c3d39849af4ece7ed9a5f971aac4e7796d7b72ab
Justin Cooper <46832618+Cooperj7@users.noreply.github.com>
2020-07-24 14:22:38 -0400

    Update urls.py

--- AC_project/AC_webapp/urls.py
+++ AC_project/AC_webapp/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
      path('', views.ph, name='Homepage'),
+     path('', views.ph, name='DataViz'),
+     path('', views.ph, name='FishFeederSettings'),
+     path('', views.ph, name='PlantBuddy'),
+     path('', views.ph, name='PumpSettings'),
+     path('', views.ph, name='Weather'),
 ]
