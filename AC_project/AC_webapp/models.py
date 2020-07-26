020c97f907e8b5b58fb59164a96eb11180c05933
Justin Cooper <46832618+Cooperj7@users.noreply.github.com>
2020-07-26 08:59:20 -0400

    updating models

--- AC_project/AC_webapp/models.py
+++ AC_project/AC_webapp/models.py
@@ -1,7 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class PumpSettings(models.Model):
+    on_time = models.IntegerField(default=0)
+    off_time = models.IntegerField(default=0)
 
+    def __str__(self):
+        return str(self.on_time) + str(off_time)
+        
+class FishFeederSettings(models.Model):
+    fishfeeder_time = models.IntegerField(default=0)
+    fishfeeder_amount = models.IntegerField(default=0)
+
+    def __str__(self):
+        return str(fishfeeder_time) + str(fishfeeder_amount)
+    
 class Weather(models.Model):
     date_time = models.CharField(max_length=40)
     temperature = models.FloatField(default=0)
@@ -13,6 +26,9 @@ class Weather(models.Model):
     cloudiness_level = models.IntegerField(default=0)
     wind_speed = models.FloatField(default=0)
 
+    def __str__(self):
+        return str(self.date_time) + str(self.temperature) + str(self.min_temperature) + str(self.max_temperature) + str(self.humidity) + str(self.weather_label) + str(self.weather_description) + str(self.cloudiness_level) + str(self.wind_speed)
+
 class SensorData(models.Model):
     water_pH = models.FloatField(default=0)
     tds = models.IntegerField(default=0)
@@ -22,3 +38,16 @@ class SensorData(models.Model):
 
     def __str__(self):
         return str(self.water_pH) + str(self.tds) + str(self.water_temp) + str(self.air_temp) + str(self.humidity)
+
+    
+class PlantBuddy(models.Model): '
+    zipcode = models.CharField(max_length=10)
+    phone_num = models.CharField(max_length=11)
+
+     def __str__(self):
+        return str(self.zipcode) + str(self. phone_num)
+    
+class DataViz(models.Model): 
+
+    
+    
