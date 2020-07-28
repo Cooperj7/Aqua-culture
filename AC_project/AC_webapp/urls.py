bddc970b8893e41e4ef086b38b30a8fd2c406573
Justin Cooper <46832618+Cooperj7@users.noreply.github.com>
2020-07-28 16:45:08 -0400

    Add files via upload

--- AC_project/AC_webapp/urls.py
+++ AC_project/AC_webapp/urls.py
@@ -2,10 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-     path('', views.ph, name='Homepage'),
-     path('', views.ph, name='DataViz'),
-     path('', views.ph, name='FishFeederSettings'),
-     path('', views.ph, name='PlantBuddy'),
-     path('', views.ph, name='PumpSettings'),
-     path('', views.ph, name='Weather'),
+     path('', views.Homepage, name='Homepage'),
 ]
